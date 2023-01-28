310c230e91134949c26ab313246be010463bb95e
Jacob Burgess <jacobburgess11@gmail.com>
2023-01-28 13:34:00 -0800

    non async

--- src/pages/api/hooks/pushed.ts
+++ src/pages/api/hooks/pushed.ts
@@ -1,7 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+const handle = (req: NextApiRequest, res: NextApiResponse) => {
   const body = req.body;
   console.log(body);
   return res.status(200).json({ message: "Hello" });
 };
+
+export default handle;
