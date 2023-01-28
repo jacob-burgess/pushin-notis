de6e3842129c839c28fb609b133a769507afdbaf
Jacob Burgess <jacobburgess11@gmail.com>
2023-01-28 13:37:21 -0800

    log

--- src/pages/api/hooks/pushed.ts
+++ src/pages/api/hooks/pushed.ts
@@ -1,8 +1,7 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 
 const handle = (req: NextApiRequest, res: NextApiResponse) => {
-  const body = req.body;
-  console.log(body);
+  console.log(req.body);
   return res.status(200).json({ message: "Hello" });
 };
 
